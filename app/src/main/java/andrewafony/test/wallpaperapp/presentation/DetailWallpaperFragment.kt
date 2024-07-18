5d53ebbdb2ffa3e77dfa73cbe67c31bf69fad5d2
Andrew <andrew.afony@yandex.ru>
2024-07-18 12:13:50 +0300

    minor optimization

--- app/src/main/java/andrewafony/test/wallpaperapp/presentation/DetailWallpaperFragment.kt
+++ app/src/main/java/andrewafony/test/wallpaperapp/presentation/DetailWallpaperFragment.kt
@@ -8,11 +8,16 @@ import android.view.ViewGroup
 import andrewafony.test.wallpaperapp.R
 import andrewafony.test.wallpaperapp.core.BaseFragment
 import andrewafony.test.wallpaperapp.databinding.FragmentDetailWallpaperBinding
+import android.graphics.drawable.Drawable
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
@@ -26,12 +31,36 @@ class DetailWallpaperFragment : BaseFragment<FragmentDetailWallpaperBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        postponeEnterTransition()
+
         lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.currentWallpaper.collectLatest { uri ->
                     Glide
                         .with(binding.root)
                         .load(uri)
+                        .listener(object : RequestListener<Drawable> {
+                            override fun onLoadFailed(
+                                e: GlideException?,
+                                model: Any?,
+                                target: Target<Drawable>,
+                                isFirstResource: Boolean
+                            ): Boolean {
+                                startPostponedEnterTransition()
+                                return false
+                            }
+
+                            override fun onResourceReady(
+                                resource: Drawable,
+                                model: Any,
+                                target: Target<Drawable>?,
+                                dataSource: DataSource,
+                                isFirstResource: Boolean
+                            ): Boolean {
+                                startPostponedEnterTransition()
+                                return false
+                            }
+                        })
                         .into(binding.fullWallpaper)
                 }
             }
